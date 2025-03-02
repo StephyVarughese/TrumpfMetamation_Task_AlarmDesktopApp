999a71951db7b713eebb439686b09c0b6a9d76c6
USER <USER@MSI>
2025-03-02 11:30:38 +0530

    Add project files.

---
+++ TrumpfMetamation_Task_AlarmDesktopApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrumpfMetamation_Task_AlarmDesktopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
